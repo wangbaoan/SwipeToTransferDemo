4223610db50bc3777392907e6a18987b6b7f290c
lijiangang <lijiangang@qq.com>
2013-04-17 10:26:36 +0800

    Initial Commit

---
+++ SwipeToTransferDemo/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  SwipeToTransferDemo
+//
+//  Created by lijiangang on 13-4-17.
+//  Copyright (c) 2013年 lijiangang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ SwipeToTransferDemo/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  SwipeToTransferDemo
+//
+//  Created by lijiangang on 13-4-17.
+//  Copyright (c) 2013年 lijiangang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
